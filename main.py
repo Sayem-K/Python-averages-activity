1882a6b2af8cb456e15b5e9db0f5abd5884ad75f
hoigoysohio <njuli@yahoo.com>
2024-12-10 17:18:43 +0000

    activity 1 done

---
+++ main.py
@@ -0,0 +1,10 @@
+tree_1 = 98
+tree_2 = 94
+tree_3 = 41
+tree_4 = 96
+tree_5 = 11
+
+total_height_of_trees = tree_1 + tree_2 + tree_3 + tree_4 + tree_5
+average_height_of_trees = total_height_of_trees / 5 
+
+print(average_height_of_trees, "inches")
